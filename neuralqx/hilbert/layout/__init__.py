9ceaf78ea05a6144442fe9f08f0066a9a023d8ff
waleed <waleed.sherif@fau.de>
2026-06-17 20:16:51 +0200

    move ranges to hilbert/local instead of layout

--- neuralqx/hilbert/layout/__init__.py
+++ neuralqx/hilbert/layout/__init__.py
@@ -12,19 +12,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-from .abstract import AbstractLocalSpace
-from .explicit import ExplicitLocalSpace
-from .factory import as_local_space
-from .range import LocalRange
-from .vector import HeterogeneousLocalSpace
-from .vector import VectorRange
-
-__all__ = [
-    "AbstractLocalSpace",
-    "ExplicitLocalSpace",
-    "HeterogeneousLocalSpace",
-    "LocalRange",
-    "VectorRange",
-    "as_local_space",
-]
